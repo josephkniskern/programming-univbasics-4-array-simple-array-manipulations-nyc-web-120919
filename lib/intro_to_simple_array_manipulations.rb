16ba6fe55562628c496a7f926791c8e2cc5c469a
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-15 18:50:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_with_args(array)
+  new_arr = array.pop(2)
+  new_arr
+end
